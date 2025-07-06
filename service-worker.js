b7fdac4c2efc747a44c45c195e8a8c4586ade301
dvdrom89 <romanodavide.ct@gmail.com>
2025-07-07 01:24:02 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
   e.waitUntil(
-    caches.open('disco-cache').then(function(cache) {
+    caches.open('disco-cache').then(function (cache) {
       return cache.addAll([
         './',
         './index.html',
@@ -10,15 +10,17 @@ self.addEventListener('install', function(e) {
         './vittoria.mp3',
         './logo1.png',
         './logo2.png',
+        './icon192.png',
+        './icon512.png',
         './manifest.json'
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
+    caches.match(e.request).then(function (response) {
       return response || fetch(e.request);
     })
   );
